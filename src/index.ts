6c239dc66c76514e10ba799a1e56e24746871bb2
P3chys <pechysadam@gmail.com>
2023-09-11 22:55:07 +0200

    Router infrastructure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-import { Elysia } from "elysia";
+import app from "./router.js";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+app.listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
